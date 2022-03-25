590d240cdc51ca9d3125080a86ce73bf4e024285
Brian Stewart <brian@bean.la>
2022-03-24 17:43:29 -0700

    update csv options to adjust for pacto csv import

--- app/use_cases/imports/import_products_from_csv_use_case.rb
+++ app/use_cases/imports/import_products_from_csv_use_case.rb
@@ -4,6 +4,14 @@ module Imports
   class ImportProductsFromCsvUseCase < UseCase
     require 'csv'
 
+    IGNORE_LINES = 5
+    COLUMN_POSITIONS = {
+      name: 0,
+      price: 1,
+      category: 2,
+      description: 3 
+    }
+
     arguments :file, :product_import_id
 
     def persist
@@ -16,9 +24,9 @@ module Imports
       record_errors = []
 
       parsed_csv.each.with_index(1) do |row, index|
-        next if product_exists?(row["name"])
+        next if product_exists?(row[COLUMN_POSITIONS[:name]])
 
-        shipping_category = find_or_create_shipping_category(row["category"])
+        shipping_category = find_or_create_shipping_category('Default')
         create_product(row, index, shipping_category, record_errors)
       end
 
@@ -34,25 +42,25 @@ module Imports
     end
 
     def parsed_csv
-      CSV.parse(file.download, headers: true, encoding: 'ISO-8859-1', col_sep: ";", skip_blanks: true).delete_if do |row|
+      CSV.parse(file.download.lines.drop(IGNORE_LINES).join("\r\n"), headers: true, encoding: 'UTF-8', col_sep: ",", skip_blanks: true).delete_if do |row|
         row.to_hash.values.all?(&:blank?)
       end
     end
 
     def create_product(row, index, shipping_category, record_errors)
       begin
+        name = row[COLUMN_POSITIONS[:name]]
         Spree::Product.transaction do
           product = Spree::Product.create!(
-            name: row["name"],
-            slug: row["slug"],
-            description: row["description"],
+            name: name,
+            stores: Spree::Store.all,
+            description: row[COLUMN_POSITIONS[:description]],
             shipping_category_id: shipping_category.id,
-            available_on: row["availability_date"],
-            price: row["price"],
-            stores: Spree::Store.all
+            available_on: Time.current,
+            price: row[COLUMN_POSITIONS[:price]]
           )
 
-          product.master.stock_items.first.update!(count_on_hand: row["stock_total"])
+          product.master.stock_items.first.update!(count_on_hand: 0)
         end
       rescue ActiveRecord::RecordInvalid => exception
         record_errors.push({ row_index: index, error_info: exception.record.errors.messages })
