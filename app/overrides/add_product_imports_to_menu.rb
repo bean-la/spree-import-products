1f558a3be2f690849d3e12686ca41a8881827f6a
Brian Stewart <brian@bean.la>
2022-03-28 18:04:49 -0700

    fix product import sidebar defaces

--- app/overrides/add_product_imports_to_menu.rb
+++ app/overrides/add_product_imports_to_menu.rb
@@ -1,6 +1,6 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-  name: 'product_imports_sidebar_menu',
-  insert_bottom: '#main-sidebar',
-  partial: 'spree/admin/shared/product_imports_sidebar_menu',
-)
+Deface::Override.new(virtual_path: 'spree/admin/shared/_main_menu',
+    name: 'add_import_products',
+    insert_bottom: 'nav',
+    partial: 'spree/admin/shared/product_imports_sidebar_menu')
+
+
