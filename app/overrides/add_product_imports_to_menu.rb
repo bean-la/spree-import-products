21ab36a504e3b0b1222a9e8340c358939abede2e
Brian Stewart <brian@bean.la>
2022-04-13 10:36:21 -0700

    temporarily disable sidebar menus

--- app/overrides/add_product_imports_to_menu.rb
+++ app/overrides/add_product_imports_to_menu.rb
@@ -1,6 +1,7 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_main_menu',
-    name: 'add_import_products',
-    insert_bottom: 'nav',
-    partial: 'spree/admin/shared/product_imports_sidebar_menu')
+# Disable for now
+# Deface::Override.new(virtual_path: 'spree/admin/shared/_main_menu',
+#     name: 'add_import_products',
+#     insert_bottom: 'nav',
+#     partial: 'spree/admin/shared/product_imports_sidebar_menu')
 
 
